47fab369534e71fb924590a87fac45ccf6040e07
Ivan <ivancernja@gmail.com>
2017-06-06 20:06:59 +0200

    added api, listview, etc

--- index.ios.js
+++ index.ios.js
@@ -9,45 +9,109 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
+  ListView,
   View
 } from 'react-native';
 
+
+var REQUEST_URL = 'https://api.myjson.com/bins/15yy8t';
+
 export default class Moviez extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+    componentDidMount() {
+      this.fetchData();
+    }
+
+    fetchData() {
+      fetch(REQUEST_URL)
+        .then((response) => response.json())
+        .then((responseData) => {
+          this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+            loaded: true,
+          });
+        })
+        .done();
+    }
+
   render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.ListView} />
+    );
+
+  }
+    
+  /*  var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  } */
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <Text>
+        Loading movies..
+      </Text>
+    </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+          <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
+        <View style={styles.rightContainer}>
+          <Text>{movie.title}</Text>
+          <Text>{movie.year}</Text>
+        </View>
       </View>
     );
   }
-}
+}  
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  rightContainer: {
+    flex: 1,
+  },
+    title: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginBottom: 8,
+    textAlign: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  year: {
+    textAlign: 'center'
   },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  ListView: {
+    paddingTop: 20,
+  }
 });
 
 AppRegistry.registerComponent('Moviez', () => Moviez);
